fec02d4b005e4b88d7cbfafb83c86e0f5e10aec1
sabler <sabler@Dynomutt.local>
2017-05-03 00:16:29 -0400

    Clone Counter - Read an array of numbers, pick out the one that appears most frequently

---
+++ js/clonecounter.js
@@ -0,0 +1,29 @@
+/** Find the most common entry in an array of numbers
+    @function clonecounter
+    @param {array} arr Simple array of numbers
+    @example clonecounter(arrayName)
+
+    @returns {string} Most common entry
+*/
+
+function clonecounter(arr) {
+    var mostCommon = 0;
+    var count = 0;
+    var compObj = {};
+        arr.forEach(function(val,idx) {
+            if(!compObj[val]) {
+                compObj[val] = 1;
+            } else {
+                compObj[val]+=1;
+            };
+        });
+
+        Object.keys(compObj).forEach(function(val,idx) {
+            if(obj[val] > count) {
+                count = compObj[val];
+                mostCommon = val;
+            }
+     });
+
+     return Number(mostCommon);
+}
