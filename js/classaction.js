fac938d8c226ed5b8bf991dbd70d83ec750c2ad3
sabler <sabler@Dynomutt.local>
2017-05-18 18:44:18 -0400

    Class Action | Add or remove CSS classes from an HTML element

---
+++ js/classaction.js
@@ -0,0 +1,27 @@
+/** Add or remove a CSS class to a specified HTML object
+    @function classAction
+    @param {object} elem HTML element
+    @param {string} action Add or Remove
+    @param {string} cssClass Arbitrary class name
+
+    @example classAction (myHTMLObject,'add','fooCss')
+*/
+
+
+function classAction (elem,action,cssClass) {
+    var classes = elem.classList.value;
+        if(action === "add") {
+            if(classes.indexOf(cssClass) === -1) {
+                let newClass = elem.className + " " cssClass;
+                elem.className = newClass;
+            }
+        }
+
+        if(action === "remove") {
+            if(classes.indexOf(cssClass) !== -1) {
+                let pattern = new RegExp('('+cssClass+' ?)+',"ig");
+                let newClass = elem.className.replace(pattern,"").trim();
+                elem.className = newClass;
+            }
+        }
+}
